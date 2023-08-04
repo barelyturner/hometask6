b5c5ccc8c41a0aa5af1c99c4ab29fd7359dddf1f
wasilii <incredibleturner@gmail.com>
2023-08-05 00:27:08 +0300

    plus func

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
+car1 = {"Toyota", "Sedan", 2.0, "Automatic", "White"}
+car2 = {"Honda", "SUV", 2.0, "Manual", "White"}
+car3 = {"Volvo", "Sedan", 2.0, "Manual", "White"}
+car4 = {"Honda", "Bus", 4.0, "Manual", "Purple"}
+car5 = {"Audi", "Coupe", 2.5, "Automatic", "Black"}
+car6 = {"Mitsubishi", "SUV", 2.0, "Manual", "Purple"}
+car7 = {"Toyota", "Coupe", 2.2, "Automatic", "White"}
+car8 = {"Mercedes", "Truck", 4.0, "Manual", "Yellow"}
 
-def common_features():
-    car1 = {"Toyota", "Sedan", 2.0, "Automatic", "White"}
-    car2 = {"Honda", "SUV", 2.0, "Manual", "White"}
-    similarity = car1.intersection(car2)
-    print(similarity)
+def common_features(a, b):
+    similarity = a.intersection(b)
+    if similarity == set():
+        print("No matches found")
+    else:
+        print(f'Actual matches: {similarity}')
 
-common_features()
+common_features(car5, car1)
